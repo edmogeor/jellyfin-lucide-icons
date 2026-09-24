212d1e9b3e48f17b149124deeb011c5db46dd37b
edmogeor <edmogeor@users.noreply.github.com>
2026-09-24 19:54:53 +0100

    fix: preserve material icon scale

--- tools/generate.mjs
+++ tools/generate.mjs
@@ -146,7 +146,7 @@ function buildCss(auditResult) {
         const lucide = resolveMaterial(material);
         if (!lucide) return [];
         const selector = `.material-icons.${material}`;
-        return `${selector} {\n  display: inline-block;\n  width: 1em;\n  height: 1em;\n  overflow: hidden;\n  font-family: inherit;\n  line-height: 1;\n  text-indent: -9999px;\n  vertical-align: -0.125em;\n  background-color: currentColor !important;\n  -webkit-mask: url("${svgDataUrl(lucide)}") center / contain no-repeat !important;\n  mask: url("${svgDataUrl(lucide)}") center / contain no-repeat !important;\n}\n\n${selector}::before {\n  content: none;\n}`;
+        return `${selector} {\n  display: inline-block;\n  width: 1em;\n  height: 1em;\n  overflow: hidden;\n  font-family: inherit;\n  line-height: 1;\n  text-indent: -9999px;\n  vertical-align: -0.125em;\n  background-color: currentColor !important;\n  -webkit-mask: url("${svgDataUrl(lucide)}") center / 1em 1em no-repeat !important;\n  mask: url("${svgDataUrl(lucide)}") center / 1em 1em no-repeat !important;\n}\n\n${selector}::before {\n  content: none;\n}`;
     });
     const muiNames = new Set([...Object.keys(muiMappings), ...auditResult.muiIcons.keys()]);
     for (const mui of [...muiNames].sort((a, b) => a.localeCompare(b))) {
@@ -159,7 +159,7 @@ function buildCss(auditResult) {
         rules.push(`${selector} {\n  background-color: currentColor !important;\n  -webkit-mask: url("${svgDataUrl(lucide)}") center / contain no-repeat !important;\n  mask: url("${svgDataUrl(lucide)}") center / contain no-repeat !important;\n}`);
     }
     for (const [selector, lucide] of seerrFinMappings) {
-        rules.push(`${selector} {\n  display: inline-block;\n  width: 1em;\n  height: 1em;\n  overflow: hidden;\n  font-family: inherit;\n  line-height: 1;\n  text-indent: -9999px;\n  vertical-align: -0.125em;\n  background-color: currentColor !important;\n  -webkit-mask: url("${svgDataUrl(lucide)}") center / contain no-repeat !important;\n  mask: url("${svgDataUrl(lucide)}") center / contain no-repeat !important;\n}\n\n${selector}::before {\n  content: none;\n}`);
+        rules.push(`${selector} {\n  display: inline-block;\n  width: 1em;\n  height: 1em;\n  overflow: hidden;\n  font-family: inherit;\n  line-height: 1;\n  text-indent: -9999px;\n  vertical-align: -0.125em;\n  background-color: currentColor !important;\n  -webkit-mask: url("${svgDataUrl(lucide)}") center / 1em 1em no-repeat !important;\n  mask: url("${svgDataUrl(lucide)}") center / 1em 1em no-repeat !important;\n}\n\n${selector}::before {\n  content: none;\n}`);
     }
     for (const [selector, lucide] of elegantFinMappings) {
         if (!existsSync(join(iconRoot, `${lucide}.svg`))) throw new Error(`Missing Lucide icon: ${lucide}`);
